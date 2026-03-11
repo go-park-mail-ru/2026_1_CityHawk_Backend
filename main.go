3587136c62599dc0b57828d9fa0e041c2580bf29
Vlados-os1 <boatslav228@gmail.com>
2026-03-11 18:08:27 +0300

    Fix home handler

--- main.go
+++ main.go
@@ -46,7 +46,6 @@ func main() {
 	mux.HandleFunc("/auth/refresh", refreshHandler(store, auth))
 	mux.HandleFunc("/auth/logout", logoutHandler(auth))
 	mux.Handle("/me", authMiddleware(auth, meHandler(store)))
-	mux.HandleFunc("/api/home", homeHandler(places))
 	mux.HandleFunc("/places", placesListHandler(places))
 	mux.HandleFunc("/api/home", homeHandler(places))
 	mux.HandleFunc("/places/", placeDetailsHandler(places))
